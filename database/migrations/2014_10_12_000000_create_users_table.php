3d0d61dd327d8246f648926cd0593abf3b463376
arindamkabir <aarindam.ahmed@northsouth.edu>
2020-02-28 03:13:08 +0600

    homepage edits

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('user_id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
-            $table->boolean('isAdmin');
+            $table->string('role')->default('customer');
+            $table->boolean('isAdmin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,19 +84,9 @@
 
     <div class="row">
 
-      <div class="col-lg-3">
 
-        <h1 class="my-4">Shop Name</h1>
-        <div class="list-group">
-          <a href="#" class="list-group-item">Category 1</a>
-          <a href="#" class="list-group-item">Category 2</a>
-          <a href="#" class="list-group-item">Category 3</a>
-        </div>
 
-      </div>
-      <!-- /.col-lg-3 -->
-
-      <div class="col-lg-9">
+      <div class="col">
 
         <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
           <ol class="carousel-indicators">
