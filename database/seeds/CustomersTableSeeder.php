dd0728bfa287cc593efae202be0d8962e3da1054
Sidratul Muntaha <Sidratul.Muntaha02@northsouth.edu>
2020-03-02 02:25:01 +0600

    Create CustomersTableSeeder.php

---
+++ database/seeds/CustomersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Int;
+
+class CustomersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('customers')->insert([
+            'user_id' => 4,
+            'location' => 'Uttara',
+        ])
+        
+        }
+    }
