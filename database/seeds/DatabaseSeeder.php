c5fa8fbe5620f248d16062346b571afd8d30ce76
arindamkabir <aarindam.ahmed@northsouth.edu>
2020-03-25 22:40:17 +0600

    added created at to seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,14 +19,14 @@ class DatabaseSeeder extends Seeder
             [ 'name' => 'Munna', 'email' => 'mnty@munna.com', 'password' => Hash::make('123456'), 'role' => '3', 'contact' => '1234567878', 'created_at' => date('Y-m-d H:i:s')],
             ]);
         DB::table('shop_owners')->insert([
-            [ 'user_id' => '1', 'location' => 'Dhanmondi', 'shop_name' => 'Shwapno'],
-            [ 'user_id' => '2', 'location' => 'Bashundhara', 'shop_name' => 'Meena Bazar'],
+            [ 'user_id' => '1', 'location' => 'Dhanmondi', 'shop_name' => 'Shwapno', 'created_at' => date('Y-m-d H:i:s')],
+            [ 'user_id' => '2', 'location' => 'Bashundhara', 'shop_name' => 'Meena Bazar', 'created_at' => date('Y-m-d H:i:s')],
         ]);
         DB::table('customers')->insert([
-            [ 'user_id' => '3', 'location' => 'Dhanmondi', 'address' => 'H-42, R-9'],
+            [ 'user_id' => '3', 'location' => 'Dhanmondi', 'address' => 'H-42, R-9', 'created_at' => date('Y-m-d H:i:s')],
         ]);
         DB::table('delivery_man')->insert([
-            [ 'user_id' => '4', 'location' => 'Dhanmondi'],
+            [ 'user_id' => '4', 'location' => 'Dhanmondi', 'created_at' => date('Y-m-d H:i:s')],
         ]);
     }
 }
