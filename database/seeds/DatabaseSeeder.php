f30fb271ae66f31458ee78716433450278d225b9
arindamkabir <aarindam.ahmed@northsouth.edu>
2020-03-25 22:17:06 +0600

    more seeds.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,9 +14,11 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('users')->insert([
             [ 'name' => 'Ayon', 'email' => 'smunt@munni.com', 'password' => Hash::make('123456'), 'role' => '2', 'contact' => '1234567878', 'created_at' => date('Y-m-d H:i:s')],
+            [ 'name' => 'Munni', 'email' => 'munty@munni.com', 'password' => Hash::make('123456'), 'role' => '2', 'contact' => '1234567878', 'created_at' => date('Y-m-d H:i:s')],
         ]);
         DB::table('shop_owners')->insert([
             [ 'user_id' => '1', 'location' => 'Dhanmondi', 'shop_name' => 'Shwapno'],
+            [ 'user_id' => '2', 'location' => 'Bashundhara', 'shop_name' => 'Meena Bazar'],
         ]);
     }
 }
