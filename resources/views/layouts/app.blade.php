63e2cc86cff985990ba42bb8ae32adf56e3a316b
Sakib Mahmud <34135349+sakib00@users.noreply.github.com>
2020-04-04 17:09:23 +0600

    nav dark

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <div class="container">
