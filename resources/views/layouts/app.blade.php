9687637484e8e5df1823b85912d74f93d8814585
arindamkabir <aarindam.ahmed@northsouth.edu>
2020-04-07 03:58:37 +0600

    navbar fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm fixed">
             <div class="container">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <div class="container">
