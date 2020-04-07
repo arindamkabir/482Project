a3408767aa73af1bf7925c4bffd36d8137784dec
Sakib Mahmud <34135349+sakib00@users.noreply.github.com>
2020-04-07 20:43:10 +0600

    Update ShopOwnerController.php

--- app/Http/Controllers/ShopOwnerController.php
+++ app/Http/Controllers/ShopOwnerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\User;
 use DB;
@@ -13,13 +14,16 @@ class ShopOwnerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id, \Auth::user()->id)
     {
         $products_shops = DB::table('shop_owners')
         ->join('products', 'products.shop_id', '=', 'shop_owners.shop_id')
+        ->join('users', 'shop_owners.user_id', '=', 'users.id')
         ->select('products.product_id',  'shop_owners.shop_id', 'products.price', 'products.stock','products.description', 'products.image', 'products.name', 'products.created_at', 'products.updated_at','shop_owners.shop_name','shop_owners.location')
+        ->select('shop_owners.customer_id',  'shop_owners.user_id', 'shop_owners.location', 'shop_owners.shop_name','users.name','users.email', 'users.contact', 'shop_owners.created_at', 'shop_owners.updated_at')
+        ->where('shop_id', $id)
         ->get();
-        
+
         return view('shopowner.home', ['products' => $products_shops]);
 
     }
