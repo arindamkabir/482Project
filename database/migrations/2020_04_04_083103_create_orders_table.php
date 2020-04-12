6af89b79fd78489b4e5fca55a5f484015450b52a
arindamkabir <51151800+arindamkabir@users.noreply.github.com>
2020-04-12 08:23:53 -0700

    delivery home page

--- database/migrations/2020_04_04_083103_create_orders_table.php
+++ database/migrations/2020_04_04_083103_create_orders_table.php
@@ -14,8 +14,9 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('order_id');
             $table->unsignedInteger('user_id');
+            $table->unsignedInteger('shop_id');
             $table->string('total');
             $table->string('order_status')->default('paid');
             $table->timestamps();
