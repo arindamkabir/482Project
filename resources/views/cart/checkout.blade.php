3833f5b942ec48faaf6b8cecb6074154efdf5498
Sakib Mahmud <sakib.mahmud01@northsouth.edu>
2021-01-05 15:24:14 +0600

    checkout bkash image and button added

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -3,19 +3,26 @@
 @section('content')
 <div class="container mt-3">
 
-@if (session('success'))
-<div class="alert alert-success">
-    {{ session('success') }}
-</div>
-@endif
-@if (session('error'))
-<div class="alert alert-danger">
-    {{ session('error') }}
-</div>
-@endif
-    <h4 class="page-heading text-center">Check out successful yo!</h4>
-  
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+    @if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+    @endif
+    <h4 class="page-heading text-center">Checkout</h4>
 
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <img src="{{asset('images/bkash-payment.gif')}}" alt="">
+        </div>
+        <div class="row justify-content-md-center">
+            <button type="button" class="btn btn-primary">Pay With Bkash</button>
+        </div>
+    </div>
 
 </div>
 
